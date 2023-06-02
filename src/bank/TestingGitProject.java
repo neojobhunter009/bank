eec4f162287f2c3d81b133f73aa1aa609ca32075
revanthpotluri <potlurirevanthchowdary@gmail.com>
2023-06-01 23:35:32 -0400

    Revanth First Commit

--- src/bank/TestingGitProject.java
+++ src/bank/TestingGitProject.java
@@ -5,7 +5,7 @@ public class TestingGitProject {
 	public static void main(String[] args) {
 		System.out.println("Bank Project");
 		System.out.println("Bank Project");
-
+		System.out.println("Awesome");
 	}
 
 }
